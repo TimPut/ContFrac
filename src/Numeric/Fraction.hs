fad2b5982f519d43f19a75b3b3e4a7b7d3daa444
timput <tim@timput.com>
2019-07-21 23:09:14 -0600

    no longer drop trivial convegents from beginning of stream

--- src/Numeric/Fraction.hs
+++ src/Numeric/Fraction.hs
@@ -1,24 +1,18 @@
 {-# LANGUAGE BangPatterns #-}
 
-module Numeric.Fraction (convergents, phi, phi', toContinued, upperBounds, lowerBounds, approximate, approximateContinued, strict) where
+module Numeric.Fraction (convergents, toContinued, upperBounds, lowerBounds, approximate, approximateContinued, strict) where
 
 import           Data.Ratio
 import           GHC.Real   (Ratio (..))
 
 -- We really do want a zero denominator here, so we use the dumb constructor from GHC.Real.
 convergents :: Integral a => [a] -> [Ratio a]
-convergents coeffs = drop 2 $ (\(_,res,_) -> res) <$> iterate conv (0 :% 1, 1 :% 0, coeffs)
+convergents coeffs = (\(_,res,_) -> res) <$> iterate conv (0 :% 1, 1 :% 0, coeffs)
   where conv (!h_2 , !h_1, (a:as)) = ( h_1
                                    , (a * numerator   h_1 + numerator h_2)
                                    % (a * denominator h_1 + denominator h_2)
                                    , as)
         conv (!h_2, !h_1, [])          = ( h_2, h_1, [])
-
-phi :: Double
-phi = (1 + sqrt 5) / 2
-
-phi' :: [Integer]
-phi' = [1,1..]
 
 toContinued :: (RealFrac t, Integral a) => t -> [a]
 toContinued x = if b /= 0 then a : toContinued (recip b) else [a]
